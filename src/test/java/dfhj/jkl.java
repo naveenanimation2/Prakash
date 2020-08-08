40535064d4f15018af1dbe563e7d7034237720a2
Prakash03 <prakashghonge03@gmail.com>
2020-08-08 15:08:42 +0530

    first commit

---
+++ src/test/java/dfhj/jkl.java
@@ -0,0 +1,17 @@
+package dfhj;
+
+import org.testng.annotations.Test;
+
+public class jkl {
+
+	
+	@Test
+	public void rty() {
+		
+		System.out.println("shgh");
+		System.out.println("shgh");
+		System.out.println("shgh");
+		System.out.println("shgh");
+		
+	}
+}
